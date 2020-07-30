3fda5db1a0f8fa07d70e57d99d259f21e141ea18
Anton Ruvinsky <46845518+Anjimoo@users.noreply.github.com>
2020-07-30 22:20:16 +0300

    Work in progress

---
+++ YearlyPrecipitation.cs
@@ -0,0 +1,19 @@
+﻿namespace BDP_Anton_Hod
+{
+    public class YearlyPrecipitation
+    {
+        public int Year { get; set; }
+        public double JanuaryPrecipitation { get; set; }
+        public double FebruaryPrecipitation { get; set; }
+        public double MarchPrecipitation { get; set; }
+        public double AprilPrecipitation { get; set; }
+        public double MayPrecipitation { get; set; }
+        public double JunePrecipitation { get; set; }
+        public double JulyPrecipitation { get; set; }
+        public double AugustPrecipitation { get; set; }
+        public double SeptemberPrecipitation { get; set; }
+        public double OctoberPrecipitation { get; set; }
+        public double NovemberPrecipitation { get; set; }
+        public double DecemberPrecipitation { get; set; }
+    }
+}

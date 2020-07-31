3a967280a050910ef759d70b90dd6a6006502648
Anton Ruvinsky <46845518+Anjimoo@users.noreply.github.com>
2020-07-31 20:50:36 +0300

    Splitted YearlyPrecipitation and SeasonPrecipitation into 2 files

---
+++ SeasonPrecipitation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BDP_Anton_Hod
+{
+    public class SeasonPrecipitation
+    {
+        public Season Season { get; set; }
+        public Dictionary<Month, double> MonthlyPrecipitation { get; } = new Dictionary<Month, double>(4);
+
+        // total season precipitation
+        public double TotalPrecipitation
+        {
+            get
+            {
+                return MonthlyPrecipitation.Sum(keyValuePair => keyValuePair.Value);
+            }
+        }
+
+        public KeyValuePair<Month, double> MaxPrecipitation
+        {
+            get
+            {
+                return MonthlyPrecipitation.Aggregate((kvp1, kvp2) => kvp1.Value > kvp2.Value ? kvp1 : kvp2);
+            }
+        }
+
+        public KeyValuePair<Month, double> MinPrecipitation
+        {
+            get
+            {
+                return MonthlyPrecipitation.Aggregate((kvp1, kvp2) => kvp1.Value < kvp2.Value ? kvp1 : kvp2);
+            }
+        }
+    }
+}
--- YearlyPrecipitation.cs
+++ YearlyPrecipitation.cs
@@ -13,37 +13,6 @@ namespace BDP_Anton_Hod
         January, February, March, May, April, June, July, August, September, October, November, December
     }
 
-    public class SeasonPrecipitation
-    {
-        public Season Season { get; set; }
-        public Dictionary<Month, double> MonthlyPrecipitation { get; } = new Dictionary<Month, double>(4);
-
-        // total season precipitation
-        public double TotalPrecipitation
-        {
-            get
-            {
-                return MonthlyPrecipitation.Sum(keyValuePair => keyValuePair.Value);
-            }
-        }
-
-        public KeyValuePair<Month, double> MaxPrecipitation
-        {
-            get
-            {
-                return MonthlyPrecipitation.Aggregate((kvp1, kvp2) => kvp1.Value > kvp2.Value ? kvp1 : kvp2);
-            }
-        }
-
-        public KeyValuePair<Month, double> MinPrecipitation
-        {
-            get
-            {
-                return MonthlyPrecipitation.Aggregate((kvp1, kvp2) => kvp1.Value < kvp2.Value ? kvp1 : kvp2);
-            }
-        }
-    }
-
     public class YearlyPrecipitation
     {
         public int Year { get; set; }
